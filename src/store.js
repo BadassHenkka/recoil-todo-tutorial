9bde5bc791dace5a5417c3c4ff416c71509b44d9
BadassHenkka <henri.kestio@gmail.com>
2020-09-25 10:11:12 +0300

    Finished recoil tutorial

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { todoListState, todoListFilterState } from './atoms/todo';
+import { filteredTodoListState, todoListStatsState } from './selectors/todo';
+
+export {
+  todoListState,
+  todoListFilterState,
+  filteredTodoListState,
+  todoListStatsState,
+};
